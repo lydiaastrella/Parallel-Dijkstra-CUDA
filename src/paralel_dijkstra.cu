8923c993b0f41ba917ccde9931be68b04cc25815
kariswr <karinaiswara.ki@gmail.com>
2020-03-27 19:52:11 +0700

    add print output func

--- src/paralel_dijkstra.cu
+++ src/paralel_dijkstra.cu
@@ -21,3 +21,22 @@ int* initializeGraf(int N, int* graf) {
 	}
 	return graf;
 }
+
+void printOutput(int* matrix, int N, std::string filename) {
+	ofstream file;
+	file.open(filename, ios::out);
+
+	if (file.is_open()) {
+		for (int i = 0; i < N; i++) {
+			file << "jarak dari node " << i << ": ";
+			for (int j = 0; j < N; j++) {
+				file << matrix[i*N + j] << " ";
+			}
+			file << endl;
+		}
+	}
+	else {
+		cout << "Error, unable to open file" << endl;
+	}
+	file.close();
+}
